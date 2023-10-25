47b9761bbd442aa82d1d4eb30a250545d8d4459f
Rimon hossain <rimonhossen333@gmail.com>
2023-10-25 20:22:44 +0600

    quick start remove

--- src/components/Header/Header2.jsx
+++ src/components/Header/Header2.jsx
@@ -15,7 +15,8 @@ const Header2 = () => {
           toggle
             ? "flex md:pl-0 fixed mt-0 pt-0 top-0 w-full bg-[#292929E6] lg:hidden justify-between items-center z-[9999]"
             : "flex md:pl-0 fixed mt-0 pt-0 top-0 w-full bg-black lg:hidden justify-between items-center z-[9999]"
-        }>
+        }
+      >
         <a href="/">
           <img
             alt="logo"
@@ -38,7 +39,8 @@ const Header2 = () => {
                   ? "relative flex flex-row items-center right-0 justify-center p-2 h-16 w-36 text-sm text-black focus:outline-none bg-white py-2 px-5"
                   : "relative invisible flex-row items-center right-0 justify-center p-2 h-16 w-36 text-sm text-black focus:outline-none bg-white py-2 px-5"
               }
-              onClick={() => setToggle(!toggle)}>
+              onClick={() => setToggle(!toggle)}
+            >
               <span className="sr-only">Open main menu</span>++ MENU
             </button>
             <div className={toggle ? "hidden" : "block"}>
@@ -61,12 +63,13 @@ const Header2 = () => {
                   <button
                     type="button"
                     className="relative flex flex-row items-center right-0 justify-center ml-20 p-2 h-16 w-36 text-sm text-white focus:outline-none bg-transparent py-2 px-5 order-last"
-                    onClick={() => setToggle(!toggle)}>
+                    onClick={() => setToggle(!toggle)}
+                  >
                     <span className="sr-only">Close main menu</span>-- CLOSE
                   </button>
                 </div>
                 <div className="chakra-accordion bg-[#292929CC] css-0">
-                  <div
+                  {/* <div
                     className="chakra-accordion__item css-1kobvuv"
                     onClick={() => setOpneColaps1(!opneColaps1)}>
                     <h2>
@@ -151,10 +154,11 @@ const Header2 = () => {
                         </a>
                       </div>
                     </div>
-                  </div>
+                  </div> */}
                   <div
                     className="chakra-accordion__item css-1dy2maw"
-                    onClick={() => setOpneColaps2(!opneColaps2)}>
+                    onClick={() => setOpneColaps2(!opneColaps2)}
+                  >
                     <h2>
                       <button
                         className="chakra-accordion__button bg-[#292929CC] hover:bg-[#292929CC] border-t-0 flex items-center css-1k491gn"
@@ -162,7 +166,8 @@ const Header2 = () => {
                         id="accordion-button-:r22:"
                         aria-expanded="false"
                         aria-controls="accordion-panel-:r22:"
-                        data-index="1">
+                        data-index="1"
+                      >
                         <span className="faded-lilac text-[16px] leading-[44px] font-light py-4 css-1eziwv">
                           About VSC
                         </span>
@@ -170,10 +175,12 @@ const Header2 = () => {
                           viewBox="0 0 24 24"
                           focusable="false"
                           className="chakra-icon chakra-accordion__icon css-186l2rg"
-                          aria-hidden="true">
+                          aria-hidden="true"
+                        >
                           <path
                             fill="currentColor"
-                            d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
+                            d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"
+                          ></path>
                         </svg>
                       </button>
                     </h2>
@@ -193,28 +200,33 @@ const Header2 = () => {
                               opacity: 0,
                               height: "0px",
                             }
-                      }>
+                      }
+                    >
                       <div
                         role="region"
                         id="accordion-panel-:r22:"
                         aria-labelledby="accordion-button-:r22:"
-                        className="chakra-accordion__panel bg-[#292929CC] text-[#F6F6F6] font-light leading-[22px] text-[14px] grid grid-cols-12 css-1hb5ode ml-4">
+                        className="chakra-accordion__panel bg-[#292929CC] text-[#F6F6F6] font-light leading-[22px] text-[14px] grid grid-cols-12 css-1hb5ode ml-4"
+                      >
                         <a
                           className="block py-2 text-white font-normal opacity-90 col-span-6"
                           aria-current="page"
-                          href="/get-started-vsc#what-is-vsc">
+                          href="/get-started-vsc#what-is-vsc"
+                        >
                           What is VSC
                         </a>
                         <a
                           className="block py-2 text-white font-normal opacity-90 col-span-6"
                           aria-current="page"
-                          href="/get-started-vsc#mobile-timeline">
+                          href="/get-started-vsc#mobile-timeline"
+                        >
                           Roadmap
                         </a>
                         <a
                           className="block py-2 text-white font-normal opacity-90 col-span-6"
                           aria-current="page"
-                          href="/get-started-vsc#data-nfts">
+                          href="/get-started-vsc#data-nfts"
+                        >
                           Data-NFTs
                         </a>
                         <a
@@ -222,25 +234,29 @@ const Header2 = () => {
                           aria-current="page"
                           target="_blank"
                           rel="noreferrer"
-                          href="https://vyvo.org/">
+                          href="https://vyvo.org/"
+                        >
                           VSC Foundation
                         </a>
                         <a
                           className="block py-2 text-white font-normal opacity-90 col-span-6"
                           aria-current="page"
-                          href="/get-started-vsc#pose">
+                          href="/get-started-vsc#pose"
+                        >
                           PoSe
                         </a>
                         <a
                           className="block py-2 text-white font-normal opacity-90 col-span-6"
                           aria-current="page"
-                          href="/get-started-vsc#ddh-platform">
+                          href="/get-started-vsc#ddh-platform"
+                        >
                           DDH Platform
                         </a>
                         <a
                           className="block py-2 text-white font-normal opacity-90 col-span-6"
                           aria-current="page"
-                          href="/get-started-vsc#tokenomics">
+                          href="/get-started-vsc#tokenomics"
+                        >
                           Tokenomics
                         </a>
                       </div>
@@ -248,7 +264,8 @@ const Header2 = () => {
                   </div>
                   <div
                     className="chakra-accordion__item css-1dy2maw"
-                    onClick={() => setOpneColaps3(!opneColaps3)}>
+                    onClick={() => setOpneColaps3(!opneColaps3)}
+                  >
                     <h2>
                       <button
                         className="chakra-accordion__button bg-[#292929CC] hover:bg-[#292929CC] border-t-0 css-1k491gn"
@@ -256,7 +273,8 @@ const Header2 = () => {
                         id="accordion-button-:r24:"
                         aria-expanded="false"
                         aria-controls="accordion-panel-:r24:"
-                        data-index="2">
+                        data-index="2"
+                      >
                         <span className="faded-lilac text-[16px] leading-[44px] font-light py-4 css-1eziwv">
                           Connect
                         </span>
@@ -264,10 +282,12 @@ const Header2 = () => {
                           viewBox="0 0 24 24"
                           focusable="false"
                           className="chakra-icon chakra-accordion__icon css-186l2rg"
-                          aria-hidden="true">
+                          aria-hidden="true"
+                        >
                           <path
                             fill="currentColor"
-                            d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
+                            d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"
+                          ></path>
                         </svg>
                       </button>
                     </h2>
@@ -287,36 +307,42 @@ const Header2 = () => {
                               opacity: 0,
                               height: "0px",
                             }
-                      }>
+                      }
+                    >
                       <div
                         role="region"
                         id="accordion-panel-:r24:"
                         aria-labelledby="accordion-button-:r24:"
-                        className="chakra-accordion__panel bg-[#292929CC] text-[#F6F6F6] font-light leading-[22px] text-[14px] grid grid-cols-12 css-1hb5ode">
+                        className="chakra-accordion__panel bg-[#292929CC] text-[#F6F6F6] font-light leading-[22px] text-[14px] grid grid-cols-12 css-1hb5ode"
+                      >
                         <a
                           className="block py-2 text-white font-normal opacity-90 col-span-6"
                           aria-current="page"
                           target="_blank"
                           rel="noreferrer"
-                          href="https://linktr.ee/vyvosmartchain">
+                          href="https://linktr.ee/vyvosmartchain"
+                        >
                           Socials
                         </a>
                         <a
                           className="block py-2 text-white font-normal opacity-90 col-span-6"
                           aria-current="page"
-                          href="/contact-us">
+                          href="/contact-us"
+                        >
                           Contact
                         </a>
                         <a
                           className="block py-2 text-white font-normal opacity-90 col-span-6"
                           aria-current="page"
-                          href="/press">
+                          href="/press"
+                        >
                           Press
                         </a>
                         <a
                           className="block py-2 text-white font-normal opacity-90 col-span-6"
                           aria-current="page"
-                          href="/business-inquiries">
+                          href="/business-inquiries"
+                        >
                           Business Inquiries
                         </a>
                       </div>
@@ -324,7 +350,8 @@ const Header2 = () => {
                   </div>
                   <div
                     className="chakra-accordion__item css-1dy2maw"
-                    onClick={() => setOpneColaps4(!opneColaps4)}>
+                    onClick={() => setOpneColaps4(!opneColaps4)}
+                  >
                     <h2>
                       <button
                         className="chakra-accordion__button bg-[#292929CC] hover:bg-[#292929CC] border-t-0 flex items-center css-1k491gn"
@@ -332,7 +359,8 @@ const Header2 = () => {
                         id="accordion-button-:r26:"
                         aria-expanded="false"
                         aria-controls="accordion-panel-:r26:"
-                        data-index="3">
+                        data-index="3"
+                      >
                         <span className="faded-lilac text-[16px] leading-[44px] font-light py-4 css-1eziwv">
                           Functions
                         </span>
@@ -340,10 +368,12 @@ const Header2 = () => {
                           viewBox="0 0 24 24"
                           focusable="false"
                           className="chakra-icon chakra-accordion__icon css-186l2rg"
-                          aria-hidden="true">
+                          aria-hidden="true"
+                        >
                           <path
                             fill="currentColor"
-                            d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
+                            d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"
+                          ></path>
                         </svg>
                       </button>
                     </h2>
@@ -363,34 +393,41 @@ const Header2 = () => {
                               opacity: 0,
                               height: "0px",
                             }
-                      }>
+                      }
+                    >
                       <div
                         role="region"
                         id="accordion-panel-:r26:"
                         aria-labelledby="accordion-button-:r26:"
-                        className="chakra-accordion__panel bg-[#292929CC] text-[#F6F6F6] font-light leading-[22px] text-[14px] grid grid-cols-12 css-1hb5ode">
+                        className="chakra-accordion__panel bg-[#292929CC] text-[#F6F6F6] font-light leading-[22px] text-[14px] grid grid-cols-12 css-1hb5ode"
+                      >
                         <a
                           className="col-span-6 py-2 block relative text-white before:content-['Coming_Soon'] before:absolute before:opacity-0 before:transition-opacity before:duration-500 hover:before:opacity-100 after:content-['Wallet'] after:duration-500 after:transition-opacity hover:after:opacity-0"
                           aria-current="page"
-                          href="#"></a>
+                          href="#"
+                        ></a>
                         <a
                           className="col-span-6 py-2 relative text-white before:content-['Coming_Soon'] before:absolute before:opacity-0 before:transition-opacity before:duration-500 hover:before:opacity-100 after:content-['Bridge'] after:duration-500 after:transition-opacity hover:after:opacity-0"
                           aria-current="page"
-                          href="#"></a>
+                          href="#"
+                        ></a>
                         <a
                           className="col-span-6 py-2 relative text-white before:content-['Coming_Soon'] before:absolute before:opacity-0 before:transition-opacity before:duration-500 hover:before:opacity-100 after:content-['Faucet'] after:duration-500 after:transition-opacity hover:after:opacity-0"
                           aria-current="page"
-                          href="#"></a>
+                          href="#"
+                        ></a>
                         <a
                           className="col-span-6 py-2 relative text-white before:content-['Coming_Soon'] before:absolute before:opacity-0 before:transition-opacity before:duration-500 hover:before:opacity-100 after:content-['Stake'] after:duration-500 after:transition-opacity hover:after:opacity-0"
                           aria-current="page"
-                          href="#"></a>
+                          href="#"
+                        ></a>
                       </div>
                     </div>
                   </div>
                   <div
                     className="chakra-accordion__item css-1dy2maw"
-                    onClick={() => setOpneColaps5(!opneColaps5)}>
+                    onClick={() => setOpneColaps5(!opneColaps5)}
+                  >
                     <h2>
                       <button
                         className="chakra-accordion__button bg-[#292929CC] hover:bg-[#292929CC] border-t-0 flex items-center css-1k491gn"
@@ -398,7 +435,8 @@ const Header2 = () => {
                         id="accordion-button-:r28:"
                         aria-expanded="false"
                         aria-controls="accordion-panel-:r28:"
-                        data-index="4">
+                        data-index="4"
+                      >
                         <span className="faded-lilac text-[16px] leading-[44px] font-light py-4 css-1eziwv">
                           Vyvo Backoffice (legacy users only)
                         </span>
@@ -406,10 +444,12 @@ const Header2 = () => {
                           viewBox="0 0 24 24"
                           focusable="false"
                           className="chakra-icon chakra-accordion__icon css-186l2rg"
-                          aria-hidden="true">
+                          aria-hidden="true"
+                        >
                           <path
                             fill="currentColor"
-                            d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
+                            d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"
+                          ></path>
                         </svg>
                       </button>
                     </h2>
@@ -429,18 +469,21 @@ const Header2 = () => {
                               opacity: 0,
                               height: "0px",
                             }
-                      }>
+                      }
+                    >
                       <div
                         role="region"
                         id="accordion-panel-:r28:"
                         aria-labelledby="accordion-button-:r28:"
-                        className="chakra-accordion__panel bg-[#292929CC] text-[#F6F6F6] font-light leading-[22px] text-[14px] grid grid-cols-12 css-1hb5ode">
+                        className="chakra-accordion__panel bg-[#292929CC] text-[#F6F6F6] font-light leading-[22px] text-[14px] grid grid-cols-12 css-1hb5ode"
+                      >
                         <a
                           target="_blank"
                           rel="noreferrer"
                           className="block py-2 text-white font-normal opacity-90 col-span-6"
                           aria-current="page"
-                          href="https://my.vyvo.com/login">
+                          href="https://my.vyvo.com/login"
+                        >
                           Login
                         </a>
                       </div>
